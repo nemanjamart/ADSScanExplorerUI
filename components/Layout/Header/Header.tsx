b29718dd68590eff18d20638d3d4149e74946b04
Philip Matsson <philip@winterway.eu>
2022-05-10 15:52:02 +0200

    Integrated ADS logo

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,10 +1,16 @@
 
 import styles from './Header.module.css'
+import Image from 'next/image'
 
 const Header = () => {
     return (
         <header className={styles.container}>
-            <text>ADS Scan Explorer</text>
+            <div className={styles.titlegroup}>
+                <div className={styles.logoContainer}>
+                    <Image className={styles.logo} src="/assets/ads.svg" alt={"ADS logotype"} width={80} height={80} />
+                </div>
+                <text>ADS Scan Explorer</text>
+            </div>
         </header>
     )
 }
