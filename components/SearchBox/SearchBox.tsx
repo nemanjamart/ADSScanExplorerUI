ead748d799d22bd15b811dca225e231fe9dd4aaa
Philip Matsson <philip@winterway.eu>
2022-05-26 17:36:41 +0200

    Implemented persistent search box component state

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,24 +1,36 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import styles from './SearchBox.module.css'
 import SearchExample from './SearchExample/SearchExample';
+import { useRouter } from 'next/router'
+
 
 interface SearchBoxProps {
     showExample?: Boolean
-    initialQuery?: string
 }
 
-const SearchBox = ({showExample = false, initialQuery = ""} : SearchBoxProps) =>{
-    const [query, setQuery] = useState<string>(initialQuery);
+const SearchBox = ({ showExample = false }: SearchBoxProps) => {
+    const [query, setQuery] = useState<string>("");
+
+    useEffect(() => {
+        sessionStorage.setItem('queryState', query)
+    }, [query]);
+
+
+    useLayoutEffect(() => {
+        if (sessionStorage.getItem('queryState')) {
+            setQuery(sessionStorage.getItem('queryState'))
+        }
+    }, [])
 
     return (
         <div className={styles.container}>
             <div className={styles.inputGroup}>
                 <input type="text" className={styles.searchBox} placeholder="Search" onChange={(e) => setQuery(e.target.value)} value={query} />
-                <Link href={{pathname: '/search', query: {q: query, page: 1, limit: 10}}} passHref>
+                <Link href={{ pathname: '/search', query: { q: query, page: 1, limit: 10 } }} passHref>
                     <button className={styles.button}>
                         <FontAwesomeIcon icon={faSearch} inverse />
                     </button>
